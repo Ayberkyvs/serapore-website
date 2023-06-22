12c90ad0988ca603ebee4de49910b22e5473d0cc
ayberksch <35408256+Ayberkyvs@users.noreply.github.com>
2023-06-22 19:46:21 +0300

    update

---
+++ js/footer.js
@@ -0,0 +1,65 @@
+document.write(`
+
+<footer class="footer">
+  <div class="inner-footer">
+    <div class="footer-1">
+      <img src="img/mainLogo.png" alt="Serapore Footer Logo">
+    </div>
+    <div class="footer-2">
+      <h3 style="text-transform: uppercase;" class="footer-location">kocaelı,turkey</h3>
+      <h5 style="text-decoration: underline;">info@serapore.com.tr</h5>
+      <h5>+90 532 421 4816 / +90 552 483 66 70</h5>
+      <h5>Hacı Halil mah.1226 sok. No:11 Daire 17 Gebze Kocaeli Türkiye,</h5>
+      <a
+        href="https://www.google.com/maps/search/Hac%C4%B1halil,+1226.+Sk.+No:11+D:17,+41400+Gebze%2FKocaeli/@40.7992707,29.4336316,18.25z?entry=ttu">
+        <h4 style="text-decoration: underline; text-transform: uppercase;">HARITADA GORUNTULE <i
+            class="fa-solid fa-location-arrow" style="text-decoration: underline;"></i></h4>
+      </a>
+    </div>
+    <div class="footer-2">
+      <h3 style="text-transform: uppercase;" class="footer-location">Sayfalar</h3>
+      <a href="main.html#aboutus">
+        <h5>Hakkımızda</h5>
+      </a>
+      <a href="main.html#services">
+        <h5>Hizmetler</h5>
+      </a>
+      <a href="urunler.html">
+        <h5>Ürünlerimiz</h5>
+      </a>
+      <a href="main.html#contact">
+        <h5>İletişim</h5>
+      </a>
+      <a data-scroll href="#main">
+        <h4 style="text-decoration: underline; text-transform: uppercase;">Ana Sayfayaya Dön <i
+            class="fa-solid fa-location-arrow" style="text-decoration: underline;"></i></h4>
+      </a>
+    </div>
+    <div class="footer-3">
+      <h3 style="text-transform: uppercase;" class="footer-location">İletişimi koparmayalım, e-posta adresini gir,
+        güncel
+        kal</h3>
+      <a href="hakkimizda.html" target="_blank">
+        <h5 style="text-decoration: underline;">KAYIT OLMAK ICIN TIKLA <i class="fa-solid fa-arrow-right"
+            style="text-decoration: underline;"></i></h5>
+      </a>
+      <div class="footer-social">
+        <h3 style="text-transform: uppercase;">Bizi Takip Et!</h3>
+        <ul>
+          <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+          <li><a href="https://youtube.com/@ErtugruLAxeL" target="_blank"><i class="fa-brands fa-youtube"></i></a>
+          </li>
+          <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <h3>&copy 2023 SeraPore Makine Sanayi Ve Ticaret Ltd, Tüm hakları saklıdır.</h3>
+    <h3 id="author-mark">powered by <a href="https://www.ayberkyavas.com" target="_blank"
+        style="color: black; text-decoration: none;">YAVAS Tech.</a></h3>
+  </div>
+</footer>
+
+`);
--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var loader = document.getElementById('loader');
-  var content = document.getElementById('content');
-
-  loader.style.transition = 'opacity 0.3s ease-in-out';
-  loader.style.opacity = 1;
-
-  function hideLoader() {
-    loader.style.opacity = 0;
-    setTimeout(function() {
-      loader.style.display = 'none';
-    }, 300);
-  }
-
-  var fadeOutInterval = setInterval(function() {
-    if (loader.style.opacity > 0) {
-      loader.style.opacity -= 0.05;
-    } else {
-      clearInterval(fadeOutInterval);
-      hideLoader();
-    }
-  }, 30);
-});
-
-
 var sayiElementleri = document.querySelectorAll(".sayi");
 
 function animasyonluArtis(sayiElementi, hedefSayi) {
